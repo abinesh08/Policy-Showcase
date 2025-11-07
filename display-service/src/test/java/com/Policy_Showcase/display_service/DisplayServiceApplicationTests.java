934198c2de92007bd01181a0ff50785ffb8bc7dc
Abinesh <abinesh.r.2019.cse@ritchennai.edu.in>
2025-11-07 11:03:57 +0530

    commit setup

---
+++ display-service/src/test/java/com/Policy_Showcase/display_service/DisplayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Policy_Showcase.display_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
